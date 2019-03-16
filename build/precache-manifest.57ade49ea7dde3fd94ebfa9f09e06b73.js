b1159cf638b340e0da4020c7ac3c4b754312a78f
Vladimir Ignatov <vlad.ignatov@gmail.com>
2019-03-16 08:34:51 -0400

    Add the build dir

---
+++ build/precache-manifest.57ade49ea7dde3fd94ebfa9f09e06b73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb973869c12196cab01e44e6facb584d",
+    "url": "/static/media/logo.bb973869.svg"
+  },
+  {
+    "revision": "21f347082f88d09d8301b411dbef67a8",
+    "url": "/static/media/logo.21f34708.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "83eaacf6b19b403b5c0d",
+    "url": "/static/js/main.450d908a.chunk.js"
+  },
+  {
+    "revision": "6a12866ade79908f3da6",
+    "url": "/static/js/2.b5545675.chunk.js"
+  },
+  {
+    "revision": "83eaacf6b19b403b5c0d",
+    "url": "/static/css/main.540bfb0e.chunk.css"
+  },
+  {
+    "revision": "f385fc465f60f2c4ce1c468727e3bde1",
+    "url": "/index.html"
+  }
+];
