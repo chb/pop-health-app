b9d252794bd6c20c1b19e0cf367b44f61b968cf7
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2019-10-09 11:41:01 -0400

    Add disabled state for checkboxes

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -8,6 +8,7 @@ export default class Checkbox extends React.Component
     static propTypes = {
         label   : PropTypes.string.isRequired,
         checked : PropTypes.oneOfType([PropTypes.bool, PropTypes.number]).isRequired,
+        disabled: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
         onChange: PropTypes.func.isRequired
     };
 
@@ -16,7 +17,8 @@ export default class Checkbox extends React.Component
         return (
             <label
                 className={"checkbox-label" + (this.props.checked ? " selected" : "") }
-                onClick={() => this.props.onChange(!this.props.checked) }
+                onClick={() => !this.props.disabled && this.props.onChange(!this.props.checked) }
+                disabled={ !!this.props.disabled }
             >
                 <i className={this.props.checked ? "fas fa-check-square" : "far fa-square"}/> { this.props.label }
             </label>
