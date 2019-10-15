31527f800785c7d4e147d50e6da0e56e2b0539ad
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2019-10-15 12:06:33 -0400

    Add store debugging middleware

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import ui             from "./ui";
 import auth           from "./auth";
 import organizations  from "./organizations";
 import measures       from "./measures";
-import payers         from "./payers"
+import payers         from "./payers";
 import measureResults from "./measureResults";
 import http           from "../http";
 
@@ -13,6 +13,30 @@ import { load as loadOrganizations } from "./organizations";
 import { load as loadPayers        } from "./payers";
 import { load as loadMeasures      } from "./measures";
 import { load as loadDataSources   } from "./dataSources";
+
+const middleWares = [ thunk ];
+
+// Create logger middleware that will log all redux action but only
+// use that in development env.
+if (process.env.NODE_ENV === "development" && console && console.groupCollapsed) {
+    let logger = _store => next => action => {
+        let result;
+        if (!action.__no_log) {
+            console.groupCollapsed(action.type);
+            console.info("dispatching", action);
+            result = next(action);
+            console.log("next state", _store.getState());
+            console.groupEnd(action.type);
+        }
+        else {
+            result = next(action);
+        }
+        return result;
+    };
+
+    middleWares.push(logger);
+}
+
 
 const store = createStore(
     combineReducers({
@@ -24,7 +48,7 @@ const store = createStore(
         payers,
         measureResults
     }),
-    applyMiddleware(thunk)
+    applyMiddleware(...middleWares)
 );
 
 export default store;
@@ -35,10 +59,4 @@ http.request("api/ui").then(data => {
     store.dispatch(loadPayers(data.payers));
     store.dispatch(loadMeasures(data.measures));
     store.dispatch(loadDataSources(data.dataSources));
-
-    // once we are done hydrating the store, set up a listener that will detect
-    // changes from various reducers
-    // store.subscribe(() => {
-    //     store.getState();
-    // });
 });
