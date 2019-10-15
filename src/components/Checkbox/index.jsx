23555e6ffaca5ca276973d201d8eb8784338f1a4
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2019-10-15 11:54:38 -0400

    The Checkbox supports radio mode

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -9,18 +9,23 @@ export default class Checkbox extends React.Component
         label   : PropTypes.string.isRequired,
         checked : PropTypes.oneOfType([PropTypes.bool, PropTypes.number]).isRequired,
         disabled: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
-        onChange: PropTypes.func.isRequired
+        onChange: PropTypes.func.isRequired,
+        radio   : PropTypes.bool
     };
 
     render()
     {
+        const iconClass = this.props.radio ?
+            (this.props.checked ? "fa fa-check-circle" : "far fa-circle") :
+            (this.props.checked ? "fas fa-check-square" : "far fa-square");
+
         return (
             <label
                 className={"checkbox-label" + (this.props.checked ? " selected" : "") }
                 onClick={() => !this.props.disabled && this.props.onChange(!this.props.checked) }
                 disabled={ !!this.props.disabled }
             >
-                <i className={this.props.checked ? "fas fa-check-square" : "far fa-square"}/> { this.props.label }
+                <i className={ iconClass }/> { this.props.label }
             </label>
         );
     }
