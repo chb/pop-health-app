e7351196917b5e4e880dab6510fe36c43aebfa9c
Vladimir Ignatov <vlad.ignatov@gmail.com>
2020-04-29 13:14:17 -0400

    Latest build

---
+++ build/precache-manifest.af05c8f4dda0d332c0e5897ee927dceb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1f2ad74e1ebec673e7b9682870d9be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "426f05b11442d10b840b",
+    "url": "/static/css/main.c5d8a042.chunk.css"
+  },
+  {
+    "revision": "6889594bfcd9ced77978",
+    "url": "/static/js/2.36a29d28.chunk.js"
+  },
+  {
+    "revision": "7eac5df8fd17c53ea42b25fbf2237fb3",
+    "url": "/static/js/2.36a29d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426f05b11442d10b840b",
+    "url": "/static/js/main.ad63b758.chunk.js"
+  },
+  {
+    "revision": "9a1a37a2d8a7fee3b385",
+    "url": "/static/js/runtime-main.3f99f91b.js"
+  },
+  {
+    "revision": "21f347082f88d09d8301b411dbef67a8",
+    "url": "/static/media/logo.21f34708.png"
+  },
+  {
+    "revision": "bb973869c12196cab01e44e6facb584d",
+    "url": "/static/media/logo.bb973869.svg"
+  }
+]);
